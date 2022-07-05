a8ac0c9e4d1506cddf52a7189d8f9e3f0d8aa4c8
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-07-05 15:32:37 +0530

    sanity setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  const [isSSR,setisSSR] = useState(true)
+
+  useEffect (() => {
+    setisSSR(false)
+  },[])
+
+  if(isSSR) {
+    return null
+  }
+
+  return (
+    <div className="">
+      Navbar 
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          Sidebar
+        </div>
+        <div className="flex flex-col h-1 gap-10 mt-4 overflow-auto videos">
+          <Component {...pageProps} />
+
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
