fff9f49bbb9a3793987f1c959d734aa2200cd0e7
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-07-06 16:47:47 +0530

    google id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,8 @@ Router.events.on("routeChangeError",  progress.finish);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_API_CLIENT_ID)
+
   // return           <Component {...pageProps} />
 
 
