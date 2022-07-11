b72b38b9d044dba6804fb23c79b8b9934429bf64
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-07-11 21:11:49 +0530

    done

--- pages/api/data/[date].ts
+++ pages/api/data/[date].ts
@@ -1,15 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { client } from '../../../utils/client'
 
-export default async function handler(
+type Data = {
+  name: string
+}
+
+export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<Data>
 ) {
-  if(req.method === 'POST')
-  {
-        res.status(200).send(req)
-  
-
-  }
+  res.status(200).json({ "name": req.query.id })
+}
 }
