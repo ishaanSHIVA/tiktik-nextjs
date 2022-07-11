ab25b13ff0cfdf9bf5c77e2470ddd05f604d8454
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-07-11 20:52:05 +0530

    api testing

--- pages/api/data/[date].ts
+++ pages/api/data/[date].ts
@@ -6,12 +6,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if(req.method === 'POST')
+  if(req.method === 'GET')
   {
-
-        res.status(200).send(req.query.id)
-      
-
+        res.status(200).send(req)
+  
 
   }
 }
