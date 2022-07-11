417da579b1ba6b37b051cc765ff708872110dc76
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-07-11 22:48:53 +0530

    done

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -83,7 +83,7 @@ const Profile = ({ data }:IProps) => {
 
 export const getServerSideProps = async ({ params:{id} }:{ params:{ id:string} }) => {
     const { data } = await axios.post(`${BASE_URL}/api/profile`,{
-        "body":{id}
+        id
     },{
       headers: {
     'Content-Type': 'application/json'
